a103f9359b03cb2a3d40dbcf969150a6dd39460e
shengyugithub <594549026@qq.com>
2019-12-19 22:34:30 +0800

    css add

--- blog/blueprints/blog.py
+++ blog/blueprints/blog.py
@@ -1,10 +1,13 @@
 # !usr/bin/env/python3
 # -*- coding:utf-8 -*-
 from flask import Blueprint, render_template
+from ..models import Post
 
 blog_bp = Blueprint('blog', __name__)
 
 @blog_bp.route('/')
 def index():
-    return render_template('base.html')
+    posts = Post.query.all()[:10]
+    print(posts)
+    return render_template('base.html', posts=posts)
 
