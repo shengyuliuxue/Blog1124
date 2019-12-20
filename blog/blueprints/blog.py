1e9ae4e89a4771ff9201b3a237552b4b76af71e5
shengyu <shengyuemail@126.com>
2019-12-20 16:11:11 +0800

    page add

--- blog/blueprints/blog.py
+++ blog/blueprints/blog.py
@@ -1,10 +1,21 @@
 # !usr/bin/env/python3
 # -*- coding:utf-8 -*-
 from flask import Blueprint, render_template
+from ..models import Post
+from flask_sqlalchemy import Pagination
 
 blog_bp = Blueprint('blog', __name__)
 
 @blog_bp.route('/')
 def index():
-    return render_template('base.html')
+    posts = Post.query.all()[:10]
+    print(posts)
+    return render_template('base.html', posts=posts)
 
+@blog_bp.route('/post/<int:page>', methods=['GET'])
+def show_post(page=1):
+    #show the post with the given
+    per_page = 10
+    pagination = Post.query.order_by(Post.id).paginate(page, per_page, error_out=True)
+    posts = pagination.items
+    return render_template('posts.html', posts=posts, pagination=pagination)
--- blog/config.py
+++ blog/config.py
@@ -2,5 +2,5 @@ import os.path
 
 class Config(object):
     rootpath = os.path.dirname(__file__)
-    SQLALCHEMY_DATABASE_URI = "sqlite:////" + os.path.join(rootpath, 'data.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(rootpath, 'data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
